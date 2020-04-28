81924f6cd1cdba4e482f28017c5f39cca8478375
Anour Hannouch <anour.hannouch@visma.com>
2020-04-28 10:24:47 +0200

    Anour was here

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 // debug
 // watermark
-//more intervals and 1 sec longer???
 
-//create element
 
+
+//snapshot function with options
 function snap() {
   gifshot.takeSnapShot({
     'numFrames': 30,
     'gifWidth': 300,
-    'gifHeight': 300,
+    'gifHeight': 300
   }, function(obj) {
     if(!obj.error) {
         var image = obj.image,
         animatedImage = document.createElement('img');
         animatedImage.src = image;
-        //document.body.appendChild(animatedImage);
 
         var table = document.getElementById("pTable");
         var row = table.insertRow(0);
@@ -23,25 +22,32 @@ function snap() {
   });
 };
 
-
+//Gifshot function with options
 function createGIF() {
   gifshot.createGIF({
     'numFrames': 30,
     'gifWidth': 300,
     'gifHeight': 300,
+
+    //Progresscallback
+      progressCallback: function (captureProgress) {
+        document.querySelector("progress").classList.remove('hidden');
+        document.querySelector("progress").value = captureProgress;
+      }  
   }, function(obj) {
     if (!obj.error) {
         var image = obj.image,
         animatedImage = document.createElement('img');
         animatedImage.src = image;
-        //document.body.appendChild(animatedImage);
-
         var table = document.getElementById("pTable");
         var row = table.insertRow(0);
         row.appendChild(animatedImage);
     }
+    document.querySelector("progress").classList.add('hidden');
+    document.querySelector("progress").value = 0;
   });
 };
+
 
 
 
